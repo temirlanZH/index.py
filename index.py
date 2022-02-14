8b6fc6f7c0d0e7401b45f8b4b81b8500a82ec219
Zhanatbek Temirlan <temirlan.zh.net@gmail.com>
2022-02-14 11:24:23 +0600

    python)

---
+++ index.py
@@ -0,0 +1,343 @@
+# Python for Begginers (This is my code)
+
+
+# Он обвязал веревку вокруг запястья мальчика, потом несколько раз вокруг широкой части руки, потом крепко привязал к ногтям. Когда он закончил, мальчик уже не мог оторвать его руку. Но он мог шевелить пальцами.
+# — А теперь, пожалуйста, сожмите в кулак все, кроме мизинца. Вы должны оставить мизинец торчащим, лежащим на столе. Отлично! Отлично! Теперь мы готовы. Правой рукой вы зажигаете зажигалку. Но один момент, пожалуйста.
+# Он быстро подошел к кровати и взял нож. Он вернулся и встал возле стола с ножом в руке.
+# — Мы все готовы? — сказал он. «Господин рефери, вы должны сказать, когда начинать».
+# — Ты готов? — спросил я мальчика.
+# 'Я готов.'
+# — А ты? — обратился к старику.
+# «Вполне готов», — сказал он, поднял нож в воздух и держал его примерно в шестидесяти сантиметрах над пальцем мальчика, готовый резать. Мальчик смотрел на это, но не реагировал, и его рот вообще не шевелился. Он лишь поднял брови и нахмурился.
+# — Хорошо, — сказал я. 'Вперед, продолжать.'
+# Мальчик сказал: «Пожалуйста, сосчитайте вслух, сколько раз я зажгу ее».
+# — Да, — сказал я. 'Я сделаю это.'
+# Большим пальцем он поднял верх зажигалки и снова большим пальцем резко повернул колесо. Появилось маленькое желтое пламя.
+# — Один! — крикнул я.
+# Он не задул пламя; он закрыл крышку зажигалки и подождал секунд пять, прежде чем открыть ее снова. Он очень сильно повернул колесо, и снова появилось маленькое пламя.
+# 'Два!'
+# Больше никто ничего не сказал. Мальчик не сводил глаз с зажигалки. Мужчина поднял нож в воздух и тоже смотрел на зажигалку.
+# 'Три!'
+# «Четыре!»
+# 'Пять!'
+# 'Шесть!'
+
+# Python Math
+# x = int (input("Сколько вам лет?: "))
+# if x <= 5:
+#     print("Привет малыш")
+# if x > 5:
+#     print("Привет друг")
+# if x >= 18:
+#     print("Здравствуйте")
+
+# a = 10
+# if (a > 5) and (a > 10) or (a < 15):
+#     print(a < 15)
+
+# Акинатор с помощью Python
+
+# жёлтый, оранжевый, персиковый, коричневый.
+# print('Выберите любой цвет который показан - Желтый, Оранжевый, Персиковый, Коричневый, Черный, Серый, Светло-Черный, Черно-Белый')
+
+# question1 = input('Ваш цвет из семейства желтых?')
+
+# if question1 == 'Да':
+#     question2 = input('Ваш цвет оранжевый или жёлтый?')
+#     if question2 == 'Да':
+#         question3 = input('Ваш цвет жёлтый?')
+#         if question3 == 'Да':
+#             print('Ваш цвет жёлтый!')
+#             quit()
+#         else:
+#             print('Ваш цвет оранжевый!')
+#             quit()
+
+
+# question1 = input('Ваш цвет из семейства темно-желтых?')
+
+# if question1 == 'Да':
+#     question2 = input('Ваш цвет коричневый или персиковый?')
+#     if question2 == 'Да':
+#         question3 = input('Ваш цвет коричневый?')
+#         if question3 == 'Да':
+#             print('Ваш цвет коричневый!')
+#             quit()
+#         else:
+#             print('Ваш цвет персиковый!')
+#             quit()
+
+# question1 = input('Ваш цвет из семейства черных?')
+
+# if question1 == 'Да':
+#     question2 = input('Ваш цвет черный или серый?')
+#     if question2 == 'Да':
+#         question3 = input('Ваш цвет черный?')
+#         if question3 == 'Да':
+#             print('Ваш цвет черный!')
+#             quit()
+#         else:
+#             print('Ваш цвет серый!')
+#             quit()
+
+# question1 = input('Ваш цвет из семейства Ярко-черных?')
+
+# if question1 == 'Да':
+#     question2 = input('Ваш цвет Светло-Черный или Черно-Белый?')
+#     if question2 == 'Да':
+#         question3 = input('Ваш цвет Светло-Черный?')
+#         if question3 == 'Да':
+#             print('Ваш цвет Светло-Черный!')
+#             quit()
+#         else:
+#             print('Ваш цвет Черно-Белый!')
+#             quit()
+
+# a = 'love'
+# b = 'you'
+# c = a + b
+# print(c)
+
+# Stroka format 
+# name = "Алексей"
+
+# ice_place = "Медео"
+
+# number = 8
+
+# boolean = "Пока нету"
+
+# no_int_nimber = 0.3145
+
+# stroka = 'Привет, меня зовут {}, я увлекаюсь программированием. Я живу в Алмате и у нас есть каток {}. Вчера мы ездили на каток вместо с друзьями, нас было {} человек. Недавно на телефон скачал себе игру Brawl Stars, наличие легендарок - {}, шанс на легу - {}.' .format(name, ice_place, number, boolean, no_int_nimber)
+# print(stroka)
+
+
+# Brawlers modules
+# brawlers = "Тик, Шелли, ЛИОН, Пэм, Барли, ЛЕАН, Биби, Фрэнк, Нита, Кольт, Бык, ЛЕОН, Ворон"
+
+# brawlers = (brawlers).lower()
+# print(brawlers[67:71])
+# from autocorrect import Speller
+
+# spell = Speller(lang="en")
+# print(spell("Telegram is the mast multifunctional manager of all. Soooon I will alsa learn how to wgite telegram botc."))
+
+# color = ["Красный", "Желтый", "Синий", "Коричневый", "Черный", "Белый"]
+
+# array and stroka
+# array = ["Яблоко", "Подсолнух", 3]
+
+# if "Яблоко" in array:
+#      print("Яблоко в картинке присутствует!")
+
+# if "Подсолнух" in array:
+#      print("Подсолнух в картинке присутствует!")
+
+# if 3 in array: 
+#     print("Число 3 присутствуют в массиве!")
+
+# Операция и методы списка
+# my_list1 = ["сок", "компот"]
+# my_list2 = [ "молоко", "кофе", "чай"]
+# my_list1.extend(my_list2)
+# my_list1[0] = "kompot"
+# my_list1.sort(reverse=True)
+# print(my_list1)
+
+# array = ["Дом 1", "Дом 2", "Дом 3", "Дом 4", "Дом 5", "Дом 6", "Дом 7", "Дом 8", "Дом 9", "Дом 10", "Дом 11", "Дом 12", "Дом 13", "Дом 14" ]
+# for i in range(0,14,3):array[i] += " Доставлено"
+# print(array)
+
+
+# 2 Модуль окончен ура!!
+
+# temirlanZH = ["шоколад Milka", "сыр Ементаль", "молоко Казахстанское", "мыло хозяйственное", "колбаса Докторская", "шоколад Nestle",
+#          "молоко Домашнее", "сыр Чедер", "Салат Айсберг", "шоколад Казахстанский", "хлеб черный", "хлеб ржаной", "хлеб пшеничный",
+#          "хлеб белый", "хлеб кирпичик", "хлеб батон"]
+
+# count = 0
+
+# for index in range(len(temirlanZH)):
+#     if "молоко" in temirlanZH[index - count] or "сыр" in temirlanZH[index - count]:
+#         temirlanZH.remove(temirlanZH[index - count])
+#         count += 1
+
+# count = 0
+# xleb_count = 0
+
+# for index in range(len(temirlanZH)):
+#     if "хлеб" in temirlanZH[index - count]:
+#         if xleb_count == 2:
+#             print(temirlanZH[index - count])
+#             xleb_count = 0
+#         else:
+#             temirlanZH.remove(temirlanZH[index - count])
+#             xleb_count += 1
+#             count += 1
+
+# print(temirlanZH)
+
+
+
+# Начинаем с 3 модуля)
+# print("Начнем с 3 модуля!")
+# Function DEV in phyton
+# def summa(a, b):
+#   return a + b
+
+# number = summa(2, 3)
+# number = number * 2
+# print(number)
+
+# def identification(name, age, work):
+#     more_18, work_good = False, False
+
+#     if work in ["Доктор", "Программист" , "Программный Инженер" , "Менеджер по продажам" , "Дизайнер" , "Веб-разработчик"]:
+#         work_good = True  
+
+#         output = 'Ваше имя {0}, ваша профессия {1}, вам больше 18 лет - {2}'.format(name, work_good, more_18)
+#         return output
+
+# print(identification("Mike", 20 , "Программный Инженер"))
+# 15 lesson in phyton lang.
+# word = input()
+# chisla = list(range(1, 11)) #1,2,3,4,5,6,7,8,9,10
+
+# def half(chisla, word):
+#     if word == "половина":
+#         print(chisla[0:5])
+#     else:
+#         print(chisla)
+
+# half(chisla, word)
+
+# def decoration_function(func):
+#     def wrapper():
+#         print("Наша Функция {} - Прошла Функция)" .format(func))
+#         func()
+#     return wrapper
+
+# @decoration_function
+# def hello_world():
+#     print("Hello World!")
+# hello_world()
+
+# some = str(input())
+# a = some[::-1]
+# if some == a:
+#   print("yes")
+# else:
+#   print("no")
+
+# name = input("What's your name?")
+# print("Hello " + name)
+
+# birth_year = input("Enter your birth year: ")
+# age = 2022 - int(birth_year)
+# print(age)
+
+# int()
+# float()
+# bool()
+# str()
+
+# first = input("First: ")
+# second = input("Second: ")
+# sum = float(first) + float(second)
+# print("Sum: " + str(sum))
+
+# first = input("First: ")
+# second = input("Second: ")
+# sum = int(first) + int(second)
+# print("Sum: " + str(sum))
+
+# course = "Python for Beginners"
+# print(course.upper())
+
+# course = "Python for Beginners"
+# print(course.lower())
+
+# course = "Python for Beginners"
+# print(course.replace('x', '4'))
+
+
+# course = "Python for Beginners"
+# print('Python'in course)
+
+# words = "This is an example!"
+# print(words.encode('This is an example!')
+
+# i = 0
+# while True:
+#     print(i**2)
+#     i += 1
+#     break
+
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n + factorial(n-1)
+
+
+# print(factorial(1000))
+
+# alex_array = []
+
+# def alexey(n):
+#     if n < 10:
+#         if n in [0, 6, 9]:
+#             return n
+#         else:
+#             return 0
+#     return alexey(n - 1) + alexey(n - 2) + alexey(n - 3)
+
+# for i in range(35):
+#     if i in [0, 6, 9]:
+#         alex_array.append(alexey(i))
+#     elif i >= 10:
+#         alex_array.append(alexey(i))
+        
+# print(alex_array)
+
+# def countBits(n):
+#     return bin(n).count("1")
+
+# def generate_hashtag(s): return '#' +s.strip().title().replace(' ','') if 0<len(s)<=140 else False
+
+# word = input()
+
+# try: 
+#     if int(word) % 2 == 0:
+#         print("Это четное число")
+#     else:
+#         print("Это нечетное число")
+
+# except ZeroDivisionError:
+#     print("Нельзя делить на ноль")
+
+# else: 
+#     print("Не сработала не одна ошибка")
+# finally:
+#     print("Это был цикл try-expect-finally")
+
+# def function():
+#   numbers = {
+#     'сто': 100, 
+#     'девяносто': 90,
+#     'двенадцать': 12,
+#     'пять': 5
+# }
+#   print(numbers.items())
+
+# slovar = {"one": 100, "two": 100, "three": 100, "four": 100, "five": 100}
+# print(slovar.items())
+
+# slovar = {"one": 100, "two": 200, "three": 200, "four": 100, "five": 200}
+
+# for i in slovar:
+#     slovar[i] = 100
+# print(slovar)
+
+
